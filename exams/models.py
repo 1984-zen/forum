fb6280641ab8857fd8641d13815e847dfdc3d146
Zen <1984.apple@gmail.com>
2020-10-28 11:41:10 +0800

    feat:create model Exam_files

--- exams/models.py
+++ exams/models.py
@@ -61,3 +61,19 @@ class Option_Users(models.Model):
             self.created_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
         self.updated_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
         return super(Option_Users, self).save(*args, **kwargs)
+
+class Exam_files(models.Model):
+    name = models.CharField(max_length=30)
+    file_path = models.CharField(max_length=255)
+    exam = models.ForeignKey(Exams, on_delete=models.CASCADE, related_name='exam_files')
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.name
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+        self.updated_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+        return super(Post_files, self).save(*args, **kwargs)
