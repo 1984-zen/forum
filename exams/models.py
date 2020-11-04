a1b3f281ae3068f5acd3a8e83087a2571d01aea4
Zen <1984.apple@gmail.com>
2020-11-04 09:48:42 +0800

    feat:model add Exam_Users table

--- exams/models.py
+++ exams/models.py
@@ -79,3 +79,19 @@ class Exam_files(models.Model):
             self.created_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
         self.updated_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
         return super(Exam_files, self).save(*args, **kwargs)
+
+class Exam_Users(models.Model):
+    user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE, related_name='exam_users')
+    exam = models.ForeignKey(Exams, on_delete=models.CASCADE, related_name='exam_users')
+    date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.user
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+        self.updated_at = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+        return super(Exam_files, self).save(*args, **kwargs)
