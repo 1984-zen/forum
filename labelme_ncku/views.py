1bab910fd4310e81d8ace79fd3dbf3c42841e06c
Zen <1984.apple@gmail.com>
2021-01-28 09:46:02 +0800

    feat:abel to generate every mask when finish the label

--- labelme_ncku/views.py
+++ labelme_ncku/views.py
@@ -21,6 +21,23 @@ def get_labelme_json_file_path(request):
             create_label_images_set(folder_path) #製作label data set
             return JsonResponse({'status': 'get labelme labelme json file path successfully'})
 
+def lblsave_white_mask(filename, lbl): #單張mask存成白底使用這個function
+    if osp.splitext(filename)[1] != '.png':
+        filename += '.png'
+    # Assume label ranses [-1, 254] for int32,
+    # and [0, 255] for uint8 as VOC.
+    if lbl.min() >= -1 and lbl.max() < 255:
+        lbl_pil = PIL.Image.fromarray(lbl.astype(np.uint8), mode='P')
+        colormap = np.ones((255, 3), dtype=float)
+        colormap[0] = [0, 0, 0]
+        lbl_pil.putpalette((colormap * 255).astype(np.uint8).flatten())
+        lbl_pil.save(filename)
+    else:
+        logger.warn(
+            '[%s] Cannot save the pixel-wise class label as PNG, '
+            'so please use the npy file.' % filename
+        )
+
 def create_label_images_set(folder_path):
     json_file = folder_path.replace('/','\\') #D:\my_labelme_project\Annotations\example_folder
  
@@ -54,39 +71,50 @@ def create_label_images_set(folder_path):
                     imageData = f.read()
                     imageData = base64.b64encode(imageData).decode('utf-8')
             img = utils.img_b64_to_arr(imageData)
-            label_name_to_value = {'_background_': 0}
-            for shape in data['shapes']:
-                label_name = shape['label']
-                if label_name in label_name_to_value:
-                    label_value = label_name_to_value[label_name]
-                else:
+            label_name_to_value = {'_background_': 0} #其中'_background_': 0为背景信息，如无特殊需要不需改变，后面则为自定义标签。例如label_name_to_value={'_background_': 0, 'circle': 1, 'cross': 2, 'tick': 3,'thick':4}
+            for shape in data['shapes']: #應該是這裡要要去分把一個label name 做成一張圖
+                label_name = shape['label'] #拿到label name
+                if label_name in label_name_to_value: #label_name_to_value是一個dict
+                    label_value = label_name_to_value[label_name] # 把label name 加入進label_value
+                else: # 如果lable name 沒有在value清單裡面的時候
                     label_value = len(label_name_to_value)
-                    label_name_to_value[label_name] = label_value
+                    label_name_to_value[label_name] = label_value #{'label_name: 1也就是label_value}
             
             # label_values must be dense
-            label_values, label_names = [], []
-            for ln, lv in sorted(label_name_to_value.items(), key=lambda x: x[1]):
-                label_values.append(lv)
-                label_names.append(ln)
+            label_values, label_names = [], [] #label_values是什麼?
+            print("====label_name_to_value.items======", label_name_to_value.items()) #dict_items([('_background_', 0), ('test', 1), ('test2', 2)])
+            for ln, lv in sorted(label_name_to_value.items(), key=lambda x: x[1]): #因為label_name_to_value這個dict無法被蝶代所以用items()讓他轉為可跌代，但會變成tuple(laabel_name1, laabel_name2, ..)
+                label_values.append(lv) #lv就是Tuple的index編號: 0, 1, 2, 3, ...
+                label_names.append(ln) #ln就是label name
             assert label_values == list(range(len(label_values)))
             
-            lbl = utils.shapes_to_label(img.shape, data['shapes'], label_name_to_value)
-            
-			
-			
-            captions = ['{}: {}'.format(lv, ln)
+            #img.shape 的.shape會是一組tuple，像是(x維度，y個元素)
+            lbl = utils.shapes_to_label(img.shape, data['shapes'], label_name_to_value) #lbl會是一個numpy array, shapes_to_label(img_shape, shapes, label_name_to_value, type='class')
+            print('===lbl====', lbl.size) #size = 4915200
+            # print('===lbl====', lbl) #lbl包含了每個label的mask
+			#[[0 0 0 ... 0 0 0]
+            #[0 0 0 ... 0 0 0]
+            #[0 0 0 ... 0 0 0]
+            #...
+            #[0 0 0 ... 0 0 0]
+            #[0 0 0 ... 0 0 0]
+            #[0 0 0 ... 0 0 0]]
+            for i in range(1, len(label_name_to_value)):
+                mask_from_lbl = (lbl==[i]).astype(np.uint8) #利用label_name_to_value去找出每一個對應的mask
+                lblsave_white_mask(osp.join(out_dir2, save_file_name + f'_label_{label_names[i]}.png'), mask_from_lbl)
+            captions = ['{}: {}'.format(lv, ln) #captions翻譯叫做字幕
                 for ln, lv in label_name_to_value.items()]
-            lbl_viz = utils.draw_label(lbl, img, captions)
+            lbl_viz = utils.draw_label(lbl, img, captions) #好像是產生有字幕的label image
              
-            PIL.Image.fromarray(img).save(out_dir2+'\\'+save_file_name+'_img.png')
+            PIL.Image.fromarray(img).save(out_dir2+'\\'+save_file_name+'_img.png') #fromarray(img)裡面的參數為一個做好的圖片，然後存在一個路徑下，這裡是存image_img.png
             #PIL.Image.fromarray(lbl).save(osp.join(out_dir2, 'label.png'))
-            utils.lblsave(osp.join(out_dir2, save_file_name+'_label.png'), lbl)
+            utils.lblsave(osp.join(out_dir2, save_file_name+'_labels.png'), lbl) #utils.lblsave(路徑, lbl) 是什麼意思?
             PIL.Image.fromarray(lbl_viz).save(out_dir2+'\\'+save_file_name+
-            '_label_viz.png')
+            '_label_viz.png') #把有字幕的label image存成image_label_viz.png
  
-            with open(osp.join(out_dir2, 'label_names.txt'), 'w') as f:
-                for lbl_name in label_names:
-                    f.write(lbl_name + '\n')
+            with open(osp.join(out_dir2, 'label_names.txt'), 'w') as f: #從檔案最裡面的路徑out_dir2去寫入到label_names.txt檔案裏面
+                for lbl_name in label_names: 
+                    f.write(lbl_name + '\n') #把label name寫進txt裡面
  
             warnings.warn('info.yaml is being replaced by label_names.txt')
             info = dict(label_names=label_names)
@@ -95,6 +123,7 @@ def create_label_images_set(folder_path):
 				
 				
 			#save png to another directory
+            #另外建立一個mask_png資料夾存mask.png檔案
             if not osp.exists(json_file + '\\' + 'mask_png'):
                 os.mkdir(json_file + '\\' + 'mask_png')
             mask_save2png_path = json_file + '\\' + 'mask_png'
