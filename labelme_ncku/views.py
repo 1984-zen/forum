f9694e429f2e7af1948db935d87e4a1d49b6d63a
Zen <1984.apple@gmail.com>
2021-03-09 10:25:42 +0800

    fix:able to distinct label_name in show_label_list, rename Tempalte to show_label_list.html, fix download npz file path, navigator can go back to show_training_list, change url to 'labelme/training_folder_name/<str:training_folder_name>'

--- labelme_ncku/views.py
+++ labelme_ncku/views.py
@@ -175,18 +175,18 @@ def show_training_list(request):
     return TemplateResponse(request, 'show_training_list.html', {'training_folder_list': training_folder_list})
 
 def show_label_list(request, training_folder_name):
-    # 用'label_name', 'input_img_id'做分組並列出有大於等於1的結果，然後再取這些分組結果每組的最大id並攤平化 ids = <QuerySet [59, 60, 61, 63]>
+    #這個ids是避免template出現重複label_name用的，沒有用到。以'label_name', 'input_img_id'做分組並列出有大於等於1的結果，然後再取這些分組結果每組的最大id並攤平化 ids = <QuerySet [59, 60, 61, 63]>
     ids = Labels.objects.values('label_name', 'input_img_id').annotate(Count('label_name')).filter(label_name__count__gte=1).annotate(Max('id')).values_list('id__max', flat = True)
 
-    #Input_img 去 LEFT JOIN Labels
-    input_img_labels = Input_imgs.objects.filter(training_folder_name = training_folder_name).prefetch_related("labels").values("img_name","labels__label_name","labels__label_pic_path", "id", "labels__input_img", "labels__npy_path")
+    #Input_img 去 LEFT JOIN Labels後去除重複的label_name
+    input_img_labels = Input_imgs.objects.filter(training_folder_name = training_folder_name).prefetch_related("labels").values("labels__label_name", "labels__label_pic_path", "labels__npy_path").distinct().values("img_name", "labels__label_name", "labels__label_pic_path", "labels__npy_path")
 
     npz_path = osp.join(settings.BASE_DIR, f'media/labelme/{training_folder_name}/{training_folder_name}.npz')
 
     if osp.isfile(npz_path):
         npz_path = f'/media/labelme/{training_folder_name}/{training_folder_name}.npz'
-        return TemplateResponse(request, 'label_list.html', {'input_img_labels': input_img_labels, 'npz_path': npz_path})
-    return TemplateResponse(request, 'label_list.html', {'input_img_labels': input_img_labels})
+        return TemplateResponse(request, 'show_label_list.html', {'input_img_labels': input_img_labels, 'npz_path': npz_path, 'training_folder_name': training_folder_name})
+    return TemplateResponse(request, 'show_label_list.html', {'input_img_labels': input_img_labels, 'training_folder_name': training_folder_name})
 
 def create_label(request):
     if request.method == "POST":
