6b095ea609efe7625e8011f56e9437019fe0e87d
Zen <1984.apple@gmail.com>
2021-05-10 12:35:47 +0800

    fix:fix condition of replace_underscore.py in templatetags

--- labelme_ncku/templatetags/replace_underscore.py
+++ labelme_ncku/templatetags/replace_underscore.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter
 def replace_underscore(value):
     check_value = value.split("_")[0]
-    if len(check_value) == 12:
+    if check_value[0] == "1" and check_value[1] == "6":
         return f'/{value.replace("_", " ").split(" ", 1)[0]}'
     else:
         return ""
